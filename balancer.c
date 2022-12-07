12743f2412286237ae7fda1c164d108008db2122
Rodrigo Causarano <rjcausarano@gmail.com>
2022-12-07 23:22:47 +0000

    Read and write working perfectly

--- balancer.c
+++ balancer.c
@@ -7,7 +7,8 @@
 void main(int argc, char *argv[]) {
 	int file_i2c;
 	int length;
-	unsigned char buffer[60] = {0};
+	unsigned char write_buffer[3] = {0};
+	unsigned char read_buffer[2] = {0};
 
 	
 	//----- OPEN THE I2C BUS -----
@@ -28,10 +29,11 @@ void main(int argc, char *argv[]) {
 	}
 
     //----- WRITE BYTES -----
-	buffer[0] = 0; // Offset address
-    buffer[1] = 77; // byte to write at offset address
-	length = 2;			//<<< Number of bytes to write
-	if (write(file_i2c, buffer, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
+	write_buffer[0] = 6; // Offset address
+    write_buffer[1] = 0; // data byte
+	write_buffer[2] = 0; // data byte
+	length = 2; //<<< Number of bytes to write
+	if (write(file_i2c, write_buffer, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
 	{
 		/* ERROR HANDLING: i2c transaction failed */
 		printf("Failed to write to the i2c bus.\n");
@@ -42,14 +44,17 @@ void main(int argc, char *argv[]) {
 	
 	
 	//----- READ BYTES -----
-	length = 1;			//<<< Number of bytes to read
-	if (read(file_i2c, buffer, length) != length)		//read() returns the number of bytes actually read, if it doesn't match then an error occurred (e.g. no response from the device)
+	length = 2;	//<<< Number of bytes to read
+	if (read(file_i2c, read_buffer, length) != length)		//read() returns the number of bytes actually read, if it doesn't match then an error occurred (e.g. no response from the device)
 	{
 		//ERROR HANDLING: i2c transaction failed
 		printf("Failed to read from the i2c bus.\n");
 	}
 	else
 	{
-		printf("Data read: %d\n", buffer[0]);
+		unsigned int val = read_buffer[1];
+		val = val << 8;
+		val += read_buffer[0];
+		printf("Data read: %d\n", val);
 	}
 }
